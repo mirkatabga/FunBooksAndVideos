29a375e70283e436bcecf54b3c2a090886c52fdd
Miroslav Hristov <mhris@softserveinc.com>
2023-02-05 21:55:14 +0200

    :rocket: add test to check publishing physical product event

--- src/Tests/FunBooksAndVideos.Application.Tests/CheckoutOrder/CheckoutOrderCommandHandlerTests.cs
+++ src/Tests/FunBooksAndVideos.Application.Tests/CheckoutOrder/CheckoutOrderCommandHandlerTests.cs
@@ -6,12 +6,19 @@ using FunBooksAndVideos.Application.Exceptions;
 using FunBooksAndVideos.Application.Features.Commands.Orders;
 using FunBooksAndVideos.Application.Models.Orders;
 using MediatR;
+using FunBooksAndVideos.Domain;
+using FunBooksAndVideos.Application.Features.Events.Orders;
+using Tests.FunBooksAndVideos.Application.Tests.Builders;
+using static FunBooksAndVideos.Application.Tests.Mothers.CustomersMother;
+using static FunBooksAndVideos.Application.Tests.Mothers.ProductsMother;
 
 namespace FunBooksAndVideos.Application.Tests;
 
 public class CheckoutOrderCommandHandlerTests
 {
+    private const string RAINBOW_STREET_1 = "Rainbow str 1";
     private readonly IMapper _mapper = AutoMapperConfig.Initialize();
+    private readonly CheckoutOrderCommandBuilder _commandBuilder = new();
 
     [Theory]
     [ClassData(typeof(CheckoutOrderCommandsWithInvalidReferences))]
@@ -77,5 +84,46 @@ public class CheckoutOrderCommandHandlerTests
         await handler.Invoking(h => h.Handle(command, CancellationToken.None))
             .Should()
             .ThrowExactlyAsync<Exceptions.ValidationException>();
+    }
+
+    [Fact]
+    public async Task Handle_CheckoutOrderCommandWithPhysicalProduct_ShouldPublishPhysicalProductsOrderedEvent()
+    {
+        var jane = GetJane();
+        var lotrHardCover = GetLOTRHardCover();
+
+        var command = _commandBuilder
+            .ForCustomer(jane)
+            .ForPhysicalProducts(RAINBOW_STREET_1, 1, lotrHardCover)
+            .Build();
+
+        var uow = new Mock<IUnitOfWork>();
+
+        uow.Setup(x => x.Customers.GetByIdAsync(jane.Id, It.IsAny<string[]>()))
+            .ReturnsAsync(jane);
+
+        uow.Setup(x => x.Products.GetByIdsAsync(It.Is<IEnumerable<Guid>>(
+                productIds => productIds.SequenceEqual(new List<Guid> { lotrHardCover.Id }))))
+            .ReturnsAsync(new List<Product> { lotrHardCover });
+
+        uow.Setup(x => x.Memberships)
+            .Returns(Mock.Of<IMembershipRepository>());
+
+        uow.Setup(x => x.Orders)
+            .Returns(Mock.Of<IOrderRepository>());
+
+        var publisher = new Mock<IPublisher>();
+
+        var commandHandler = new CheckoutOrderCommandHandler(
+            uow.Object,
+            _mapper,
+            new CheckoutOrderCommandConsistencyValidator(uow.Object),
+            publisher.Object);
+
+        var orderVm = await commandHandler.Handle(command, CancellationToken.None);
+
+        publisher.Verify(
+            expression: p => p.Publish(new PhysicalProductsOrderedEvent(orderVm.Id), It.IsAny<CancellationToken>()),
+            times: Times.Once);
     }
 }
